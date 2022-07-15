b6f30e31e745393003a8aa653ecd96ae90dce8c5
Liping_Chen <Liping_Chen@epam.com>
2022-07-15 14:16:29 +0800

    chore: any -> unknown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export const isFalsy = (value: any) => value === 0 ? false : !value
+export const isFalsy = (value: unknown) => value === 0 ? false : !value
 // 在一个函数里， 改变传入的对象本身是不好的
 // 里面没有用到别的hook就函数就挺好
 export const cleanObject = (object: object) => {
@@ -37,7 +37,8 @@ export const useMount = (callback: () => void)=> {
 // log()
 // log()
 // delay可以不传不然就传number类型
-export const useDebounce = (value: any, delay?: number) => {
+// 后面用泛型来规范类型
+export const useDebounce = (value: unknown, delay?: number): any=> {
     const [debounceValue, setDebounceValue] = useState(value)
     useEffect(() => {
         // 每次在value变化以后，设置一个定时器
