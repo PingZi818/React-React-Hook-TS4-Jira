35c62e83554bc9a860da52c960cfc45919772b97
Liping_Chen <Liping_Chen@epam.com>
2022-07-15 10:12:19 +0800

    feat useMount 和 useDebounce 两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,27 +2,28 @@ import { useEffect, useState } from "react"
 import React from "react"
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
-import { cleanObject } from "../../utils"
+import { cleanObject, useMount, useDebounce } from "../../utils"
 import * as qs from 'qs';
 const apiUrl = process.env.REACT_APP_API_URL
 export const ProjectListScreen = () => {
     const [users, setUsers] = useState([])
     const [param, setParam] = useState({ name: '', personId: '' });
     const [list, setList] = useState([])
+    const debounceParam = useDebounce(param, 2000)
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
             if(response.ok) {
                 setList(await response.json())
             }
         })
-    }, [param])
-    useEffect(() => {
+    }, [debounceParam])
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async response => {
             if(response.ok) {
                 setUsers(await response.json())
             }
         })
-    }, [])
+    })
     return <div>
         <SearchPanel param ={param} setParam = {setParam} users={users}/>
         <List list={list} users={users}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,8 @@
+import { useEffect, useState } from "react"
+
 export const isFalsy = (value) => value === 0 ? false : !value
 // 在一个函数里， 改变传入的对象本身是不好的
+// 里面没有用到别的hook就函数就挺好
 export const cleanObject = (object) => {
     const result = {...object}
     Object.keys(result).forEach(key => {
@@ -8,5 +11,37 @@ export const cleanObject = (object) => {
             delete result[key]
         }
     })
+
     return result
+}
+export const useMount = (callback)=> {
+    useEffect(() => {
+        callback()
+    }, [])
+}
+// const debounce = (func, delay) => {
+//     let timeout;
+//     return (...param) => {
+//         if (timeout) {
+//             clearTimeout(timeout)
+//         }
+//         timeout = setTimeout(() => {
+//             func(...param)
+//         }, delay)
+//     }
+// }
+// const log = debounce(() => console.log('call'), 5000)
+// log()
+// log()
+// log()
+export const useDebounce = (value, delay) => {
+    const [debounceValue, setDebounceValue] = useState(value)
+    useEffect(() => {
+        // 每次在value变化以后，设置一个定时器
+        const timeout = setTimeout(() => setDebounceValue(value), delay)
+        // 每次在上一个useEffect处理完以后再运行
+        return () => clearTimeout(timeout)
+    }, [value, delay])
+
+    return debounceValue
 }
