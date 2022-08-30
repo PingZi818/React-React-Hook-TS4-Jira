d1090537f6e7ceb4451f7e3565ea03a20808cb89
Liping_Chen <Liping_Chen@epam.com>
2022-08-30 10:11:20 +0800

    feat: thunk

--- src/store/auth.slice.ts
+++ src/store/auth.slice.ts
@@ -25,12 +25,8 @@ export const authSlice = createSlice({
 const { setUser } = authSlice.actions;
 
 export const selectUser = (state: RootState) => state.auth.user;
-
-export const login = (form: AuthForm) => (dispatch: AppDispatch) =>
-  auth.login(form).then((user) => dispatch(setUser(user)));
-export const register = (form: AuthForm) => (dispatch: AppDispatch) =>
-  auth.register(form).then((user) => dispatch(setUser(user)));
-export const logout = () => (dispatch: AppDispatch) =>
-  auth.logout().then(() => dispatch(setUser(null)));
-export const bootstrap = () => (dispatch: AppDispatch) =>
-  bootstrapUser().then((user) => dispatch(setUser(user)));
+// React-Thunk
+export const login = (form: AuthForm) => (dispatch: AppDispatch) => auth.login(form).then((user) => dispatch(setUser(user)));
+export const register = (form: AuthForm) => (dispatch: AppDispatch) => auth.register(form).then((user) => dispatch(setUser(user)));
+export const logout = () => (dispatch: AppDispatch) => auth.logout().then(() => dispatch(setUser(null)));
+export const bootstrap = () => (dispatch: AppDispatch) => bootstrapUser().then((user) => dispatch(setUser(user)));
