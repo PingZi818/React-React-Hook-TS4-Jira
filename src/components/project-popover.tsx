408daf8cd34499ea2a6c58d48305206dd03d32d9
Liping_Chen <Liping_Chen@epam.com>
2022-09-02 13:33:52 +0800

    chore: fetch

--- src/components/project-popover.tsx
+++ src/components/project-popover.tsx
@@ -1,25 +1,42 @@
 import styled from "@emotion/styled";
 import { Divider, List, Popover, Typography } from "antd";
-import React from "react";
+import { ButtonNoPadding } from "components/lib";
 import { useProjectModal } from "screens/project-list/util";
 import { useProjects } from "utils/project";
-import { ButtonNoPadding } from "./lib";
+
 export const ProjectPopover = () => {
-    const {data: projects} = useProjects()
-    const { open } = useProjectModal()
-    const pinnedProjects = projects?.filter(project => project.pin)
-    const content = <ContentContainer>
-        <Typography.Text type={"secondary"}>收藏项目</Typography.Text>
-        <List>
-            {pinnedProjects?.map(project => <List.Item key={project.id}>
-                <List.Item.Meta title={project.name}></List.Item.Meta>
-            </List.Item>)}
-        </List>
-        <Divider/>
-        <ButtonNoPadding type={'link'} onClick={open}>创建项目</ButtonNoPadding>
+  const { open } = useProjectModal();
+  const { data: projects, refetch } = useProjects();
+  const pinnedProjects = projects?.filter((project) => project.pin);
+
+  const content = (
+    <ContentContainer>
+      <Typography.Text type={"secondary"}>收藏项目</Typography.Text>
+      <List>
+        {pinnedProjects?.map((project) => (
+          <List.Item key={project.id}>
+            <List.Item.Meta title={project.name} />
+          </List.Item>
+        ))}
+      </List>
+      <Divider />
+      <ButtonNoPadding onClick={open} type={"link"}>
+        创建项目
+      </ButtonNoPadding>
     </ContentContainer>
-    return <Popover placement={"bottom"} content={content}><span>项目</span></Popover>
-}
+  );
+
+  return (
+    <Popover
+      onVisibleChange={() => refetch()}
+      placement={"bottom"}
+      content={content}
+    >
+      <span>项目</span>
+    </Popover>
+  );
+};
+
 const ContentContainer = styled.div`
-min-width: 30rem
-`
+  min-width: 30rem;
+`;
