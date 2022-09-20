3ecd4671b9ee04fa1e3ce61ffcbc04813264aac0
Liping_Chen <Liping_Chen@epam.com>
2022-09-20 10:34:21 +0800

    fix: bug fix

--- __json_server_mock__/middleware.js
+++ __json_server_mock__/middleware.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) =>{
   console.log(req, 'req');
-  if(req.method==='POST' && req.path==='/login') {
+  if(req.method==='POST' && req.url==='/login') {
     if(req.body.username === 'admin' && req.body.password === '123456') {
       return res.status(200).json({
         user: {
